46eefbe4934ee64031d7d17a1ec247a4afc56ee7
Astna17 <hei.antsa.17@gmail.com>
2024-09-23 16:09:39 +0300

    feat: implement two endpoints PUT and GET

---
+++ src/main/java/com/api/rest/patrimoine/Controller/PatrimoineController.java
@@ -0,0 +1,50 @@
+package com.api.rest.patrimoine.Controller;
+
+import com.api.rest.patrimoine.Model.Patrimoine;
+import com.api.rest.patrimoine.Service.PatrimoineService;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/patrimoines")
+public class PatrimoineController {
+    private final PatrimoineService patrimoineService;
+
+    public PatrimoineController(PatrimoineService patrimoineService) {
+        this.patrimoineService = patrimoineService;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patrimoine> createOrUpdatePatrimoine(@PathVariable String id,
+                                                               @RequestBody Patrimoine patrimoine) {
+        if (patrimoine.getPossesseur() == null || patrimoine.getPossesseur().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        patrimoine.setDerniereModification(LocalDateTime.now());
+
+        Patrimoine existingPatrimoine = patrimoineService.getPatrimoineById(id);
+
+        Patrimoine updatedPatrimoine = patrimoineService.createOrUpdatePatrimoine(id, patrimoine);
+
+        if (existingPatrimoine != null) {
+            return ResponseEntity.ok(updatedPatrimoine);
+        } else {
+            return ResponseEntity.status(HttpStatus.CREATED).body(updatedPatrimoine);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patrimoine> patrimoineParId(@PathVariable String id) {
+        Patrimoine patrimoine = patrimoineService.getPatrimoineById(id);
+        if (patrimoine == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(patrimoine);
+    }
+
+}
